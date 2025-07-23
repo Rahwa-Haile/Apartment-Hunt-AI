1757d5250264f458418f94bffab828202b9e2d37
Rahwa Yohannes <rahwahaile578@gmail.com>
2025-07-22 22:43:25 -0400

    feat: wip for calling openai

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import './App.css';
+import OpenAI from 'openai';
+import {useEffect} from "react"
 
+
+const client = new OpenAI();
 function App() {
+  
+  useEffect(() =>{
+    const loadGPT = async () => {
+      const response = await client.responses.create({
+        model: 'gpt-4.1',
+        input: 'Write a one-sentence bedtime story about a unicorn.',
+      })
+    }
+  },[]);
+
+ 
+
+  console.log(response.output_text);
   return <div> Hello World </div>;
 }
 
