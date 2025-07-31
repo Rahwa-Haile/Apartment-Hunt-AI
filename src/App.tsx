0b8e4a3954f5d7fd24af6d383fe25e8e30f0a49c
Rahwa Yohannes <rahwahaile578@gmail.com>
2025-07-31 16:50:15 -0400

    fix: comment out unused variables to pass lint checks

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import './App.css';
-import OpenAI from 'openai';
+// import OpenAI from 'openai';
 import Chat from './Components/Pages/Chat';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const client = new OpenAI({
-  apiKey: import.meta.env.VITE_API_KEY,
-  dangerouslyAllowBrowser: true,
-});
-async function loadGPT() {
-  console.log('calling openai');
-  const response = await client.responses.create({
-    model: 'gpt-4.1',
-    input: 'apartment listings"',
-  });
+// const client = new OpenAI({
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   dangerouslyAllowBrowser: true,
+// });
+// async function loadGPT() {
+//   console.log('calling openai');
+//   const response = await client.responses.create({
+//     model: 'gpt-4.1',
+//     input: 'apartment listings"',
+//   });
 
-  console.log(response.output_text);
-}
-// loadGPT();
+//   console.log(response.output_text);
+// }
+// // loadGPT();
 function App() {
   return (
     <div>
