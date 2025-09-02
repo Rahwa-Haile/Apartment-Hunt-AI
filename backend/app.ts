3e5e5dc932fe44d0f2a95ce1c7eba96cd5f18df1
Rahwa Yohannes <rahwahaile578@gmail.com>
2025-09-02 15:15:24 -0400

    feat: setup MySQL connection and test route

--- backend/app.ts
+++ backend/app.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from 'express';
 const app = express();
+import { connectDB } from './db/connectDB';
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hi this is you home page');
+const PORT: number = 5000;
+
+app.get('/', async (req: Request, res: Response) => {
+  const [rows] = await connectDB.query('SELECT 1+1 AS result');
+  res.json({ result: rows });
 });
 
 app.listen(5000, () => {
-  console.log('app is running on port 5000');
+  console.log('app is listening at port 5000');
 });
