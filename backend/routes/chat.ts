ca57482d557f9fe1f9e959e7a3fd2289c0596d82
Rahwa Yohannes <rahwahaile578@gmail.com>
2025-09-08 15:09:55 -0400

    feat: implement CRUD operations for Chats

--- backend/routes/chat.ts
+++ backend/routes/chat.ts
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.get('/', getAllChat);
 router.post('/', createChat);
-router.get(':id', getChat);
-router.delete(':id', deleteChat)
+router.get('/:id', getChat);
+router.delete('/:id', deleteChat)
 
 
 export default router;
