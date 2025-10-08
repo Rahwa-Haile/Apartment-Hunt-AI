3f59f13c533d92be8d3ac370b5f562e784734023
Rahwa Yohannes <rahwahaile578@gmail.com>
2025-10-08 17:11:04 -0400

    feat(database): add Listings table with relevant columns and indexes

--- backend/db/createTables.sql
+++ backend/db/createTables.sql
@@ -23,4 +23,37 @@ CREATE TABLE IF NOT EXISTS Messages(
 );
 
 
-   
+
+CREATE TABLE IF NOT EXISTS Listings (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    rentcast_id VARCHAR(255) UNIQUE,
+    formattedAddress VARCHAR(255),
+    addressLine1 VARCHAR(255),
+    addressLine2 VARCHAR(255),
+    city VARCHAR(100),
+    state CHAR(2),
+    stateFips VARCHAR(10),
+    zipCode VARCHAR(20),
+    county VARCHAR(100),
+    countyFips VARCHAR(10),
+    latitude DECIMAL(10,6),
+    longitude DECIMAL(10,6),
+    propertyType VARCHAR(50),
+    bedrooms INT,
+    bathrooms DECIMAL(3,1),
+    squareFootage INT,
+    status VARCHAR(50),
+    price DECIMAL(10,2) ,
+    listingType VARCHAR(50),
+    listedDate DATETIME,
+    removedDate DATETIME,
+    createdDate DATETIME,
+    lastSeenDate DATETIME,
+    daysOnMarket INT,
+     INDEX idx_bedrooms (bedrooms),
+    INDEX idx_bathrooms (bathrooms),
+    INDEX idx_squareFootage (squareFootage),
+    INDEX idx_price (price),
+    INDEX idx_daysOnMarket (daysOnMarket)
+);
+
